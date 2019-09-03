e9b73d001a7ee7bd90bd96a7cae9bbfc58d90d5c
vincentmylee <vincentmylee@gmail.com>
2019-09-03 21:09:58 +0800

    remove unused import

--- src/test/java/com/rp/lib/unittest/flow/calculatorTest.java
+++ src/test/java/com/rp/lib/unittest/flow/calculatorTest.java
@@ -3,8 +3,6 @@ package com.rp.lib.unittest.flow;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class calculatorTest {
 
     @Test
